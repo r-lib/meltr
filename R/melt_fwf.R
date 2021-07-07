e66576a223111c7e83b4655731ae1a75a196ebee
Jim Hester <james.f.hester@gmail.com>
2021-07-07 12:02:42 -0400

    Include the fwf helpers as well

--- R/melt_fwf.R
+++ R/melt_fwf.R
@@ -57,3 +57,75 @@ melt_fwf <- function(file, col_positions,
   )
   warn_problems(out)
 }
+
+#' @rdname melt_fwf
+#' @export
+#' @param n Number of lines the tokenizer will read to determine file structure. By default
+#'      it is set to 100.
+fwf_empty <- function(file, skip = 0, skip_empty_rows = FALSE, col_names = NULL, comment = "", n = 100L) {
+  ds <- datasource(file, skip = skip, skip_empty_rows = skip_empty_rows)
+
+  out <- whitespaceColumns(ds, comment = comment, n = n)
+  out$end[length(out$end)] <- NA
+
+  col_names <- fwf_col_names(col_names, length(out$begin))
+  out$col_names <- col_names
+  out
+}
+
+#' @rdname read_fwf
+#' @export
+#' @param widths Width of each field. Use NA as width of last field when
+#'    reading a ragged fwf file.
+#' @param col_names Either NULL, or a character vector column names.
+fwf_widths <- function(widths, col_names = NULL) {
+  pos <- cumsum(c(1L, abs(widths)))
+  fwf_positions(pos[-length(pos)], pos[-1] - 1L, col_names)
+}
+
+#' @rdname read_fwf
+#' @export
+#' @param start,end Starting and ending (inclusive) positions of each field.
+#'    Use NA as last end field when reading a ragged fwf file.
+fwf_positions <- function(start, end = NULL, col_names = NULL) {
+  stopifnot(length(start) == length(end))
+  col_names <- fwf_col_names(col_names, length(start))
+
+  tibble(
+    begin = start - 1L,
+    end = end, # -1 to change to 0 offset, +1 to be exclusive,
+    col_names = as.character(col_names)
+  )
+}
+
+
+#' @rdname read_fwf
+#' @export
+#' @param ... If the first element is a data frame,
+#'   then it must have all numeric columns and either one or two rows.
+#'   The column names are the variable names. The column values are the
+#'   variable widths if a length one vector, and if length two, variable start and end
+#'   positions. The elements of `...` are used to construct a data frame
+#'   with or or two rows as above.
+fwf_cols <- function(...) {
+  x <- lapply(list(...), as.integer)
+  names(x) <- fwf_col_names(names(x), length(x))
+  x <- tibble::as_tibble(x)
+  if (nrow(x) == 2) {
+    res <- fwf_positions(as.integer(x[1, ]), as.integer(x[2, ]), names(x))
+  } else if (nrow(x) == 1) {
+    res <- fwf_widths(as.integer(x[1, ]), names(x))
+  } else {
+    stop("All variables must have either one (width) two (start, end) values.",
+      call. = FALSE
+    )
+  }
+  res
+}
+
+fwf_col_names <- function(nm, n) {
+  nm <- nm %||% rep("", n)
+  nm_empty <- (nm == "")
+  nm[nm_empty] <- paste0("X", seq_len(n))[nm_empty]
+  nm
+}
--- R/reexports.R
+++
@@ -1,3 +0,0 @@
-#' @importFrom readr fwf_empty fwf_widths fwf_positions fwf_cols
-#' @export fwf_empty fwf_widths fwf_positions fwf_cols
-NULL
